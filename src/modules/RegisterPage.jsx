2dd229b0973edba4f7b2e8d38eda409f8f1201ea
Arun Kumar <arunselvaraj191@gmail.com>
2026-04-18 19:48:26 +0530

    Register page changes

--- src/modules/RegisterPage.jsx
+++ src/modules/RegisterPage.jsx
@@ -1,14 +1,86 @@
 import { useEffect } from "react";
+import { TextField, Box, Typography, Button } from "@mui/material";
+import { useNavigate } from "react-router-dom";
 
 const RegisterPage = () => {
+  const navigate = useNavigate();
   useEffect(() => {
     console.log("Register Page Mounted");
   }, []);
   return (
-    <div>
-      <h1>Register Page</h1>
-      <p>Welcome to the registration page!</p>
-    </div>
+    <Box>
+      <Typography variant="h4" sx={{ color: "primary.main" }}>
+        Register Page
+      </Typography>
+      <Box>
+        <Box></Box>
+        <TextField
+          label="Username"
+          placeholder="Enter your username"
+          variant="outlined"
+          margin="normal"
+          sx={{
+            width: "50%",
+          }}
+        />
+      </Box>
+      <Box>
+        <TextField
+          label="Email"
+          placeholder="Enter your email"
+          variant="outlined"
+          margin="normal"
+          sx={{
+            width: "50%",
+          }}
+        />
+      </Box>
+      <Box>
+        <TextField
+          label="Password"
+          placeholder="Enter your password"
+          variant="outlined"
+          margin="normal"
+          type="password"
+          sx={{
+            width: "50%",
+          }}
+        />
+      </Box>
+      <Box>
+        <TextField
+          label="Confirm Password"
+          placeholder="Confirm your password"
+          variant="outlined"
+          margin="normal"
+          type="password"
+          sx={{
+            width: "50%",
+          }}
+        />
+      </Box>
+      <Box>
+        <Button
+          variant="contained"
+          color="primary"
+          sx={{ mt: 2, width: "50%" }}
+          size="medium"
+        >
+          Register
+        </Button>
+      </Box>
+      <Box>
+        <Button
+          variant="outlined"
+          color="primary"
+          sx={{ mt: 2, width: "50%" }}
+          size="medium"
+          onClick={() => navigate("/")}
+        >
+          Go to Login
+        </Button>
+      </Box>
+    </Box>
   );
 };
 
